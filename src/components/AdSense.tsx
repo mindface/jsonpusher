8814b3699da01b9c6c555a5ee021d7764e4350bd
mindface <in00und@gmail.com>
2024-10-31 16:25:51 +0900

    settings load ads

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -5,20 +5,26 @@ import { usePathname } from "next/navigation";
 export default function AdSense() {
 	const pathName = usePathname();
   useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.log(err);
-    }
+    const script = document.createElement("script");
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7006357860917162";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+		if(typeof window !== "undefined" && window.adsbygoogle) {
+			try {
+				(window.adsbygoogle = window.adsbygoogle || []).push({});
+			} catch (err) {
+				console.log(err);
+			}
+		}
+    return () => {
+      document.body.removeChild(script);
+    };
   }, [pathName]);
 	return (
 		<div className="ad-sense" key={pathName}>
-			<script
-				async
-				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7006357860917162"
-     		crossOrigin="anonymous">	
-			</script>
-			<ins className="adsbygoogle"
+
+			<ins
 				style={{display:"block"}}
 				data-ad-client="ca-pub-7006357860917162"
 				data-ad-slot="1234567890"
