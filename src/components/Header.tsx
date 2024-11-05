cb33546d6451700a59bf26e0fec5fa5e6f9d89a1
mindface <in00und@gmail.com>
2024-11-05 16:00:12 +0900

    settings header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,76 +1,8 @@
 import Link from "next/link";
 import { Menu } from "../stories/Menu/Menu";
 
-const pathList = [
-	{
-		pathId: 1,
-		path: "/",
-		name: "ホーム",
-	},
-	{
-		pathId: 2,
-		path: "/health",
-		name: "健康",
-	},
-	{
-		pathId: 3,
-		path: "/sports",
-		name: "スポーツ",
-	},
-];
-
-const menuList = [
-	{
-		pathId: "menu01",
-		path: "/",
-		name: "ホーム",
-	},
-	{
-		pathId: "menu02",
-		path: "/levelUpPattern",
-		name: "レベルアップのパターン",
-	},
-	{
-		pathId: "menu03",
-		path: "/levelSkillComparison",
-		name: "スキルを比較するということ",
-	},
-	{
-		pathId: "menu04",
-		path: "/planFeedback",
-		name: "練習を計画すること",
-	},
-	{
-		pathId: "menu05",
-		path: "/nextPlanEvaluation",
-		name: "計画の評価を文字にする",
-	},
-	{
-		pathId: "menu06",
-		path: "/growthQuantification",
-		name: "成長を定量化する",
-	},
-	{
-		pathId: "menu07",
-		path: "/practiceInformation",
-		name: "練習と情報量の変化について",
-	},
-	{
-		pathId: "menu08",
-		path: "/differencesResults",
-		name: "微妙な差と結果からのフィードバックについて",
-	},
-	{
-		pathId: "menu09",
-		path: "/useAdvice",
-		name: "アドバイスを使うことと評価",
-	},
-	{
-		pathId: "menu10",
-		path: "/updateInformationExperience",
-		name: "経験から情報を更新",
-	}
-];
+import pathList from "../json/menuPathList.json";
+import menuList from "../json/menuInfoList.json";
 
 export default function Header() {
 	return (
