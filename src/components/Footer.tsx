4918583f46806cee766308dba114b07baedee2ce
mindface <in00und@gmail.com>
2024-11-07 16:14:26 +0900

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import FooterLogo from "../assets/images/footer_logo.png";
+
+export default function Footer() {
+	return (
+		<footer className="p-6 pb-4 ">
+			<div className="left-box flex">
+				<div className="logo-box flex items-center justify-center min-h-screen w-full">
+					<div className="bg-white">
+						<Image
+							src={FooterLogo}
+							className="rounded-lg"
+							width={340}
+							height={340}
+							alt="health image"
+							style={{ width: "auto", objectFit: "cover" }}
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="text-center">
+				<small>&copy; meliru</small>
+			</div>
+		</footer>
+	);
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,38 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import { Menu } from "../stories/Menu/Menu";
 
 import pathList from "../json/menuPathList.json";
 import menuList from "../json/menuInfoList.json";
 
+import Logo from "../assets/images/logo.png";
+
 export default function Header() {
 	return (
 		<div className="flex items-center justify-between shadow-lg p-6 pb-4 ">
-			<div className="links">
-				{pathList.map((item) => (
-					<Link
-						key={item.pathId}
-						href={item.path}
-						className="inline-block mr-2 p-2 leading-none rounded-lg transition-colors link"
-					>
-						{item.name}
-					</Link>
-				))}
+			<div className="left-box flex items-center">
+				<div className="logo-box">
+					<Image
+						src={Logo}
+						className="rounded-lg"
+						width={40}
+						height={40}
+						alt="health image"
+						style={{ width: "auto", objectFit: "cover" }}
+					/>	
+				</div>
+				<div className="links">
+					{pathList.map((item) => (
+						<Link
+							key={item.pathId}
+							href={item.path}
+							className="inline-block mr-2 p-2 leading-none rounded-lg transition-colors link"
+						>
+							{item.name}
+						</Link>
+					))}
+				</div>
 			</div>
 			<Menu pathList={menuList} size="large" />
 		</div>
