65d1b34abb3b78508a90b4090b8200ac56cc6de4
mindface <in00und@gmail.com>
2024-12-12 14:53:32 +0900

    settings info

---
+++ src/app/api/claude/route.tsx
@@ -0,0 +1,47 @@
+import { Anthropic } from '@anthropic-ai/sdk';
+import { NextResponse, type NextRequest } from "next/server";
+import {
+	sanitaizeText,
+} from "../../../lib/convertString";
+
+const client = new Anthropic({
+  apiKey: process.env.ANTHROPIC_API_KEY
+});
+
+export async function GET() {
+	try {
+		return NextResponse.json({ name: "Contact Api" });
+	} catch (error) {
+		throw error;
+	}
+}
+
+export async function POST(reqest: NextRequest) {
+	const { prompt } = await reqest.json();
+	const _prompt = sanitaizeText(prompt);
+	console.log(prompt);
+	try {
+		const result = await client.messages.create({
+			model: "claude-3-5-sonnet-20241022",
+			max_tokens: 1000,
+			temperature: 0,
+			system: "短い詩でのみ応答してください。",
+			messages: [
+				{
+					"role": "user",
+					"content": [
+						{
+							"type": "text",
+							"text": prompt
+						}
+					]
+				}
+			]
+		});
+		console.log('Response:', result.content);
+		return NextResponse.json({ message: "Success", status: 200, content: result.content });
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json({ message: "Failed", status: 500, content: "no content" });
+	}
+}
---
+++ src/components/Claude.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { useState } from "react";
+import { Title3h } from "../stories/title3h/Title3h";
+import { Button } from "../stories/Button/Button";
+import { Textarea } from "../stories/TextArea/Textarea";
+import { Dialog } from "../stories/Dialog/Dialog";
+
+import ContentPlanFeedback from "./ContentPlanFeedback"
+
+import { useStoreSportsText } from "../store/sportText"
+import { useStoreHealthText } from "../store/healthText"
+
+import { GeminiResponse } from "../type/apiResponse";
+
+export default function Claude() {
+	// const baseTextSize = 300;
+	const [sendTitle, sendTitleSet] = useState("");
+	const [_displayedChunks, _displayedChunksSet] = useState<string>("");
+	const { sportsText } = useStoreSportsText();
+	const { healthText } = useStoreHealthText();
+
+	const textChange = (text: string) => {
+		const regex1 = /\*\s\*\*(.*?)\*\*/g;
+		const regex2 = /\*\*(.*?)\*\*/g;
+		return text.replace(regex1, "<br /><br /><h2>$1</h2>").replace(regex2, "<h2>$1</h2>");
+	}
+
+	const fetchGeminiApi = async () => {
+		const res = await fetch("/api/gamini",{
+			method: "POST",
+			body: JSON.stringify({ prompt: sendTitle })
+		});
+		const data = await res.json();
+		console.log(data);
+		if(data.status <= 200){
+			_displayedChunksSet(textChange(data.content));
+		}
+	}
+
+	return (
+		<div className={["gemini"].join(" ")}>
+			<div className="pb-2">
+				<Title3h title="ai [Claude] に質問する" />
+				<div className="pb-4">
+					<div className="flex flex-wrap">
+						<div className="mr-4">
+							<Dialog
+								label="計画を確認する"
+								type="button"
+								className=""
+								onChange={() => {}}
+							>
+								<ContentPlanFeedback />
+							</Dialog>
+						</div>
+						<div className="mr-4">
+							<Dialog
+								label="スポーツの質問テキストを確認する"
+								type="button"
+								onChange={() => {}}
+							>
+								<div className="p-8">
+									{sportsText !== "" ? sportsText : "質問を作成してください。"}
+								</div>
+							</Dialog>
+						</div>
+						<div>
+							<Dialog
+								label="健康の質問テキストを確認する"
+								type="button"
+								onChange={() => {}}
+							>
+								<div className="p-8">
+									{healthText !== "" ? healthText : "質問を作成してください。"}
+								</div>
+							</Dialog>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="pb-4">
+				<div className="pb-8">
+					<Textarea
+					  className="w-full"
+						placeholder=""
+					  value={sendTitle}
+						onChange={(value) => { sendTitleSet(value as string); }}
+					/>
+				</div>
+				<p className="pb-2">
+					<Button
+						label="質問する"
+						size="small"
+					  onClick={() => { fetchGeminiApi(); }}
+					/>
+				</p>
+				<div className="view-content pb-4">
+					<p className="pb-4">aiの返答が返ってきます。</p>
+					{/* 出力するパタン次第で採用 */}
+					{/* {displayedChunks.map((chunk, index) => (
+						<div key={index} style={{ marginBottom: '10px', padding: '10px', borderRadius: '5px' }}>
+							{chunk}
+						</div>
+					))} */}
+					<div className="put-text"
+						dangerouslySetInnerHTML={{
+							__html: _displayedChunks
+						}}
+					></div>
+				</div>
+			</div>
+		</div>
+	);
+}
