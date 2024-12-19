4be67c1b641172dd2c450f3d303bf6bacce8bac5
mindface <in00und@gmail.com>
2024-12-19 20:57:42 +0900

    settings build error

--- src/components/SectionMakeCycle.tsx
+++ src/components/SectionMakeCycle.tsx
@@ -1,36 +1,14 @@
 "use client"
-import { useState } from "react";
 import { Titleline3h } from "../stories/Titleline3h/Titleline3h";
-import { Button } from "../stories/Button/Button";
-import { Input } from "../stories/Input/Input";
 
 import CMakeCycleIEdit from "./CMakeCycleIEdit";
 import CMakeCycleList from "./CMakeCycleList";
-
-import { Cycle } from "../type/cycle";
 
 import { useStoreCycle } from "../store/cycle";
 
 // ToDo 今後firebaseと連携してく際にコンポーネントを分ける
 export default function SectionMakeCycle() {
 	const { cycles } = useStoreCycle();
-	const [cycleCardItem, cycleCardItemSet] = useState<Cycle[]>([]);
-
-	const [cycleCardItemTitle, cycleCardItemTitleSet] = useState("");
-	const [cycleCardItemDetail, cycleCardItemDetailSet] = useState("");
-
-	const addCycleCardItemAction = () => {
-		const setItem = {
-			id: cycleCardItemTitle.length+1,
-			title: cycleCardItemTitle,
-			detail: cycleCardItemDetail,
-			connectId: "0",
-			userId: "0"
-		}
-		cycleCardItemSet([...cycleCardItem,setItem]);
-	}
-	const editCycleCardItemAction = () => {}
-	const deleteCycleCardItemAction = () => {}
 
 	return (
 		<section className="section-make-cycle">
