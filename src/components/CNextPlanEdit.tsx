b9ff9257ed93209335c8f73d594c31b7a64d6c08
mindface <in00und@gmail.com>
2025-01-05 15:18:50 +0900

    settings yarn build

--- src/components/CNextPlanEdit.tsx
+++ src/components/CNextPlanEdit.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button } from "../stories/Button/Button";
 import { Input } from "../stories/Input/Input";
 
