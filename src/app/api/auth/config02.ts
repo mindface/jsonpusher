1056229a854d541f69394a58cbb6176e96052569
mindface <in00und@gmail.com>
2025-01-07 16:30:36 +0900

    change deploy settings

--- src/app/api/auth/config02.ts
+++ src/app/api/auth/config02.ts
@@ -1,48 +1,48 @@
-import NextAuth, { type NextAuthConfig } from "next-auth";
-import type { Provider } from "next-auth/providers";
-import GoogleProvider from "next-auth/providers/google";
+// import NextAuth, { type NextAuthConfig } from "next-auth";
+// import type { Provider } from "next-auth/providers";
+// import GoogleProvider from "next-auth/providers/google";
 
-const { SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+// const { SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 
-if (!SECRET) throw new Error("You must provide SECRET env var.");
-if (!GOOGLE_CLIENT_ID) throw new Error("You must provide GOOGLE_ID env var.");
-if (!GOOGLE_CLIENT_SECRET)
-	throw new Error("You must provide GOOGLE_SECRET env var.");
+// if (!SECRET) throw new Error("You must provide SECRET env var.");
+// if (!GOOGLE_CLIENT_ID) throw new Error("You must provide GOOGLE_ID env var.");
+// if (!GOOGLE_CLIENT_SECRET)
+// 	throw new Error("You must provide GOOGLE_SECRET env var.");
 
-const providers: Provider[] = [
-	GoogleProvider({
-		clientId: GOOGLE_CLIENT_ID,
-		clientSecret: GOOGLE_CLIENT_SECRET,
-		authorization: {
-			params: {
-				redirect_uri: `${process.env.NEXT_PUBLIC_BASIC_URL}/api/auth/callback/google`,
-			},
-		},
-	}),
-];
+// const providers: Provider[] = [
+// 	GoogleProvider({
+// 		clientId: GOOGLE_CLIENT_ID,
+// 		clientSecret: GOOGLE_CLIENT_SECRET,
+// 		authorization: {
+// 			params: {
+// 				redirect_uri: `${process.env.NEXT_PUBLIC_BASIC_URL}/api/auth/callback/google`,
+// 			},
+// 		},
+// 	}),
+// ];
 
-export const authOptions: NextAuthConfig = {
-	providers: providers,
-	secret: SECRET,
-	callbacks: {
-		async session({ token, session }) {
-			if (token.sub && session.user) {
-				session.user.id = token.sub;
-			}
-			// 確認次第実装
-			// if (token.role && session.user) {
-			//   session.user.role = token.role as any;
-			// }
-			return session;
-		},
-		async jwt({ token }) {
-			if (!token.sub) return token;
-			// const existingUser = await getUserById(token.sub);
-			// if (!existingUser) return token;
-			// token.role = existingUser.role;
-			return token;
-		},
-	},
-};
+// export const authOptions: NextAuthConfig = {
+// 	providers: providers,
+// 	secret: SECRET,
+// 	callbacks: {
+// 		async session({ token, session }) {
+// 			if (token.sub && session.user) {
+// 				session.user.id = token.sub;
+// 			}
+// 			// 確認次第実装
+// 			// if (token.role && session.user) {
+// 			//   session.user.role = token.role as any;
+// 			// }
+// 			return session;
+// 		},
+// 		async jwt({ token }) {
+// 			if (!token.sub) return token;
+// 			// const existingUser = await getUserById(token.sub);
+// 			// if (!existingUser) return token;
+// 			// token.role = existingUser.role;
+// 			return token;
+// 		},
+// 	},
+// };
 
-export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
+// export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
