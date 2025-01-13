44f29a98db828dde41f3ff55abb01d0191cd7422
mindface <in00und@gmail.com>
2025-01-13 22:26:40 +0900

    settings carender style

--- src/components/SectionMemoryView.tsx
+++ src/components/SectionMemoryView.tsx
@@ -5,15 +5,18 @@ import { Dialog } from "../stories/Dialog/Dialog";
 
 import { useStoreMemoery } from "../store/memory";
 
-import FullCalendar from '@fullcalendar/react';
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import jaLocale from '@fullcalendar/core/locales/ja';
+import jaLocale from "@fullcalendar/core/locales/ja";
 import { ForMatter } from "../utils/formater";
-import { EventClickArg } from '@fullcalendar/core';
+import { EventClickArg } from "@fullcalendar/core";
+
+import styles from "../styles/calendar.module.css";
 
 type EventItem = {
 	id: string;
 	title: string;
+	detail: string;
 	start: string;
 	end: string;
 	color: string;
@@ -23,6 +26,7 @@ export default function SectionMemoryView() {
 	const { memories, getMemory } = useStoreMemoery();
   const [selectedEvent, setSelectedEvent] = useState<{
     title: string;
+		detail: string;
     start: string;
     end?: string;
   } | null>(null);
@@ -50,19 +54,20 @@ export default function SectionMemoryView() {
 	const carenderEvent = (item: EventClickArg) => {
     setSelectedEvent({
       title: item.event.title || '無題のイベント',
+			detail: item.event.extendedProps.detail || '不明な詳細',
       start: ForMatter.formatDay(item.event.start ?? new Date) ?? '不明な開始日時',
       end: ForMatter.formatDay(item.event.start ?? new Date) ?? '不明な日時',
     });
 		setDialogOpen(!dialogOpen);
 	}
 
- const carenderEvents = useMemo(() => {
-	console.log(memories);
+ const carenderEvents = useMemo<EventItem[]>(() => {
 	return memories.map((memory) => ({
 		id: String(memory.id),
 		title: memory.title,
+		detail: memory.detail,
 		start: ForMatter.convertTimestampToDayjs(memory.createAt),
-		end: ForMatter.convertTimestampToDayjs(memory.createAt),
+		end: ForMatter.convertTimestampToDayjs(memory.updateAt),
 		color: "blue",
 	}));
  },[memories]);
@@ -88,16 +93,25 @@ export default function SectionMemoryView() {
 					locales={[jaLocale]}
 					locale="ja"
 					eventClick={carenderEvent}
+					eventClassNames={(info) => {
+						if (info.event.title === 'test001') {
+							return [styles.eventHighlight];
+						}
+						return "rounded-lg p-1 cursor-pointer";
+					}}
 				/>
 				<Dialog
 					type="none"
 					ounterActionValue={dialogOpen}
 				>
-					<div className="inner">
-						<h3>イベント詳細</h3>
-						<p><strong>タイトル:</strong> {selectedEvent?.title}</p>
-						<p><strong>開始日時:</strong> {selectedEvent?.start}</p>
-						{selectedEvent?.end && <p><strong>終了日時:</strong> {selectedEvent?.end}</p>}
+					<div className="inner p-4 border rounded-lg">
+						<h3 className="pb-4">記録の詳細</h3>
+						<p className="pb-4"><strong>タイトル:</strong> {selectedEvent?.title}</p>
+						<div className="pb-4 max-h-[240px]"><strong>詳細:</strong> {selectedEvent?.detail}</div>
+						<div className="flex">
+							<p className="mr-4 pb-4"><strong>開始日時:</strong> {selectedEvent?.start}</p>
+							{selectedEvent?.end && <p  className="pb-4"><strong>更新日時:</strong> {selectedEvent?.end}</p>}
+						</div>
 					</div>
 				</Dialog>
 			</div>
