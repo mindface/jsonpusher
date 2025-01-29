d7b7908a8ca57b27850f4910678f8f22e25e2e5b
mindface <in00und@gmail.com>
2025-01-30 08:58:10 +0900

    setting menu.tsx

--- src/stories/Menu/Menu.tsx
+++ src/stories/Menu/Menu.tsx
@@ -27,33 +27,43 @@ export const Menu = ({
 	const [menuSwitch, menuSwitchSet] = useState(false);
 	const menuAction = () => {
 		menuSwitchSet(!menuSwitch);
+		if(!menuSwitch) {
+			document.body.setAttribute("style","overflow: hidden;");
+		}else {
+			document.body.setAttribute("style","");
+		}
 	};
 	return (
-		<div
-			className={[
-				`menu--${size}`,
-				className,
-				menuSwitch ? "open" : "close",
-			].join(" ")}
-			{...props}
-		>
-			<button className="menu relative rounded-lg" onClick={menuAction}>
-				<span className="icon"></span>
-				<span className="icon"></span>
-				<span className="icon"></span>
-			</button>
-			<div className="menu-box fixed top-40 right-0 overflow-hidden pt-2 pb-2">
-				{pathList.map((path) => (
-					<p className="menu-item" key={path.pathId}>
-						<Link
-							className="menu-link link leading-none whitespace-nowrap overflow-hidden text-ellipsis p-4"
-							href={path.path}
-						>
-							{path.name}
-						</Link>
-					</p>
-				))}
+		<>
+		  {menuSwitch && <div className="overlay"
+			  onClick={menuAction}
+			></div>}
+			<div
+				className={[
+					`menu--${size}`,
+					className,
+					menuSwitch ? "open" : "close",
+				].join(" ")}
+				{...props}
+			>
+				<button className="menu relative rounded-lg" onClick={menuAction}>
+					<span className="icon"></span>
+					<span className="icon"></span>
+					<span className="icon"></span>
+				</button>
+				<div className="menu-box fixed top-40 right-0 overflow-hidden pt-2 pb-2">
+					{pathList.map((path) => (
+						<p className="menu-item" key={path.pathId}>
+							<Link
+								className="menu-link link leading-none whitespace-nowrap overflow-hidden text-ellipsis p-4"
+								href={path.path}
+							>
+								{path.name}
+							</Link>
+						</p>
+					))}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
