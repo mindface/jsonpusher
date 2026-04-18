7c361a5849ab21275b295a513c3e3dd408b5e882
takanori kamaguchi <37267958+mindface@users.noreply.github.com>
2026-04-18 13:18:01 +0900

    check auth api4 (#47)
    Co-authored-by: takanori <asdfghjkl@takanoris-Mac-mini.local>

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,8 @@ export async function middleware(request: NextRequest) {
 	const token = await getToken({
 		req: request,
 		secret: process.env.NEXTAUTH_SECRET,
+		secureCookie: true,
+		cookieName: "__Secure-authjs.session-token",
 	});
 	console.log("middleware token", token);
 	console.log("All Cookies:", request.cookies.getAll());
