3c2315db56c1b622ba337c577efb1e561f8b4c87
takanori <asdfghjkl@takanoris-Mac-mini.local>
2026-04-18 13:12:26 +0900

    check auth api4

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,8 @@ export async function middleware(request: NextRequest) {
 	const token = await getToken({
 		req: request,
 		secret: process.env.NEXTAUTH_SECRET,
+		secureCookie: true,
+		cookieName: "__Secure-authjs.session-token",
 	});
 	const protectedRoutes = [
 		"/",
